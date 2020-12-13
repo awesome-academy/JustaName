b11c06e739c3a9b466f24de3f47e1e5824901492
D2720 <ti3ndat363@gmail.com>
2020-12-13 23:51:15 +0700

    model laptop

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,96 @@ Rails.application.routes.draw do
   devise_for :users,
     controllers:{omniauth_callbacks: "users/omniauth_callbacks"}
   root 'staticpages#home'
-  resources :laptops, only: [:new, :create, :show, :destroy]
+
+  resources :laptops, only: [:new, :create, :show, :index, :destroy]
+
+  get '/macbook', to: "laptops#macbook"
+  get '/macbook-pro', to: "laptops#macbook_pro"
+  get '/macbook-air', to: "laptops#macbook_air"
+  get '/macbook-pro-2020', to: "laptops#macbook_pro_2020"
+  get '/macbook-pro-2019', to: "laptops#macbook_pro_2019"
+  get '/macbook-pro-2018', to: "laptops#macbook_pro_2018"
+  get '/macbook-pro-2017', to: "laptops#macbook_pro_2017"
+  get '/macbook-pro-2016', to: "laptops#macbook_pro_2016"
+  get '/macbook-pro-2015', to: "laptops#macbook_pro_2015"
+  get '/macbook-pro-2014', to: "laptops#macbook_pro_2014"
+  get '/macbook-pro-m1', to: "laptops#macbook_pro_m1"
+  get '/macbook-air-2020', to: "laptops#macbook_air_2020"
+  get '/macbook-air-2019', to: "laptops#macbook_air_2019"
+  get '/macbook-air-2018', to: "laptops#macbook_air_2018"
+  get '/macbook-air-2017', to: "laptops#macbook_air_2017"
+  get '/macbook-air-2016', to: "laptops#macbook_air_2016"
+  get '/macbook-air-2015', to: "laptops#macbook_air_2015"
+  get '/macbook-air-2014', to: "laptops#macbook_air_2014"
+  get '/macbook-air-m1', to: "laptops#macbook_air_m1"
+
+  get '/surface', to: "laptops#surface"
+  get '/surface-pro', to: "laptops#surface_pro"
+  get '/surface-laptop', to: "laptops#surface_laptop"
+  get '/surface-book', to: "laptops#surface_book"
+  get '/surface-go', to: "laptops#surface_go"
+  get '/surface-pro-7', to: "laptops#surface_pro_7"
+  get '/surface-pro-6', to: "laptops#surface_pro_6"
+  get '/surface-pro-5', to: "laptops#surface_pro_5"
+  get '/surface-pro-4', to: "laptops#surface_pro_4"
+  get '/surface-pro-x', to: "laptops#surface_pro_x"
+  get '/surface-laptop-3', to: "laptops#surface_laptop_3"
+  get '/surface-laptop-2', to: "laptops#surface_laptop_2"
+  get '/surface-laptop-1', to: "laptops#surface_laptop_1"
+  get '/surface-laptop-go', to: "laptops#surface_laptop_go"
+  get '/surface-book-3', to: "laptops#surface_book_3"
+  get '/surface-book-2', to: "laptops#surface_book_2"
+  get '/surface-book-1', to: "laptops#surface_book_1"
+  get '/surface-go-2', to: "laptops#surface_go_2"
+  get '/surface-go-1', to: "laptops#surface_go_1"
+
+  get '/dell', to: "laptops#dell"
+  get '/dell-alienware', to: "laptops#dell_alienware"
+  get '/dell-inspiron', to: "laptops#dell_inspiron"
+  get '/dell-precision', to: "laptops#dell_precision"
+  get '/dell-gaming', to: "laptops#dell_gaming"
+  get '/dell-latitude', to: "laptops#dell_latitude"
+  get '/dell-xps', to: "laptops#dell_xps"
+
+  get '/razer', to: "laptops#razer"
+  get '/razer-blade-stealth-13', to: "laptops#razer_blade_stealth_13"
+  get '/razer-blade-15', to: "laptops#razer_blade_15"
+  get '/razer-blade-pro-17', to: "laptops#razer_blade_pro_17"
+
+  get '/asus', to: "laptops#asus"
+  get '/asus-expertbook', to: "laptops#asus_expertbook"
+  get '/asus-vivobook', to: "laptops#asus_vivobook"
+  get '/asus-gaming', to: "laptops#asus_gaming"
+  get '/asus-zenbook', to: "laptops#asus_zenbook"
+
+  get '/lg', to: "laptops#lg"
+  get '/lg-gram', to: "laptops#lg_gram"
+
+  get '/hp', to: "laptops#hp"
+  get '/hp-elitebook', to: "laptops#hp_elitebook"
+  get '/hp-pavilion', to: "laptops#hp_pavilion"
+  get '/hp-spectre', to: "laptops#hp_spectre"
+  get '/hp-envy', to: "laptops#hp_envy"
+  get '/hp-probook', to: "laptops#hp_probook"
+  get '/hp-zbook', to: "laptops#hp_zbook"
+
+  get '/acer', to: "laptops#acer"
+  get '/acer-aspire', to: "laptops#acer_aspire"
+  get '/acer-predator', to: "laptops#acer_predator"
+  get '/acer-nitro', to: "laptops#acer_nitro"
+  get '/acer-swift', to: "laptops#acer_swift"
+
+  get '/lenovo', to: "laptops#lenovo"
+  get '/lenovo-ideapad', to: "laptops#lenovo_ideapad"
+  get '/lenovo-thinkpad', to: "laptops#lenovo_thinkpad"
+  get '/lenovo-thinkpad-x', to: "laptops#lenovo_thinkpad_x"
+  get '/lenovo-legion', to: "laptops#lenovo_legion"
+  get '/lenovo-thinkpad-t', to: "laptops#lenovo_thinkpad_t"
+  get '/lenovo-workstation', to: "laptops#lenovo_workstation"
+
+  get '/msi', to: "laptops#msi"
+  get '/msi-gaming', to: "laptops#msi_gaming"
+
 end
 # as :user do
 #   get "signin" => "devise/sessions#new"
--- db/schema.rb
+++ db/schema.rb
@@ -9,6 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2020_12_13_081902) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -44,6 +45,11 @@ ActiveRecord::Schema.define(version: 2020_12_13_081902) do
     t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -60,13 +66,21 @@ ActiveRecord::Schema.define(version: 2020_12_13_081902) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "laptops", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.string "price"
-    t.integer "brand"
+  create_table "models", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.bigint "brand_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["brand_id"], name: "index_models_on_brand_id"
+  end
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "model_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["model_id"], name: "index_products_on_model_id"
   end
 
   create_table "rates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -105,5 +119,7 @@ ActiveRecord::Schema.define(version: 2020_12_13_081902) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "models", "brands"
+  add_foreign_key "products", "models"
   add_foreign_key "reviews", "users"
 end
