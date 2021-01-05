a052b4a8ef1d43f093f247cca0f3180d2df3799a
D2720 <ti3ndat363@gmail.com>
2021-01-05 10:36:31 +0700

    Product nested form for photo

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,9 +1,18 @@
 class Price < ApplicationRecord
 
-  belongs_to :product
-  has_many :configurations, dependent: :destroy
+  	belongs_to :product
+  	has_many :configurations, dependent: :destroy
 
-  validates :price, presence: true
-  validates :product_id, presence: true
+  	validates :price, presence: true
+
+  	accepts_nested_attributes_for :configurations, allow_destroy: true, reject_if: :all_blank
+
+  	rails_admin do
+	    nested do
+	      	configure :product do
+	        	hide
+	      	end
+	    end
+	end
 
 end
