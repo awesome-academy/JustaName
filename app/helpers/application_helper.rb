1eb0c659c7cc3af38b1dcde79827a34c19116f87
D2720 <ti3ndat363@gmail.com>
2021-01-26 14:37:59 +0700

    create order complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,14 @@
 module ApplicationHelper
 
-  include Pagy::Frontend
+	include Pagy::Frontend
 
-  def full_title(page_title = '')
-    base_title = "Tuan Monkey "
-	if page_title.empty?
-	  base_title
-	else
-	  "#{page_title} | #{base_title}"
+	def full_title(page_title = '')
+	    base_title = "Tuan Monkey "
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
 	end
-  end
   
 end
